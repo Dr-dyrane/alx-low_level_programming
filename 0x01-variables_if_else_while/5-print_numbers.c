d668a4169ee47f32dfd6e40592d94d24d1c884d7
Dr-dyrane <halodyrane@gmail.com>
2023-02-17 10:18:32 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entrt point
+ *
+ * Return: always return 0
+ *
+ */
+int main(void)
+{
+	int a = 0;
+
+	for (; a < 10 ; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
