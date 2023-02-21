d82bb48ea4213777fe1320379c548530a6374ebc
Dr-dyrane <halodyrane@gmail.com>
2023-02-21 15:47:22 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha -function that checks for alkl aplhabets
+ *
+ * @c: paramerer to be checked
+ *
+ * Return: 1 if is an alphabet
+ * and 0 otherwise
+ *
+ */
+
+int _isalpha(int c)
+{
+	if ((c > 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
