67e2de4259e021a4ded38f3b1eb23f53ad7cdc18
Dr-dyrane <halodyrane@gmail.com>
2023-02-21 07:32:06 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints the string "_putchar" followed bg a new line.
+ *
+ * Return: 0 on success.
+ */
+int main(void)
+{
+	char c[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(c[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
