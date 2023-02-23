6eaafa3c9b729f0b4bca795a8dc61d1d67fc0de9
Dr-dyrane <halodyrane@gmail.com>
2023-02-23 10:07:55 -0800

    betty-10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -2,10 +2,13 @@
 /**
  * print_triangle - draws a triangle using multiple terminal lines and #'s.
  * @size: the height of the triangles.
+ *
+ * Return: void
  */
 void print_triangle(int size)
 {
 	int i, j;
+
 	if (size > 0)
 	{
 		for (i = 1; i <= size; i++)
