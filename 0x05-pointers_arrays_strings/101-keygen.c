d1e1abcf9d7627bd875983627432aef2ddee6b14
Dr-dyrane <halodyrane@gmail.com>
2023-02-27 09:35:34 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,9 +1,35 @@
-#import "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
- * main -
- * Return:
+ * main - generate valid random passwords for the crackme program.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
+	int sum = 2772;
+	int n;
+	
+	srand(time(0));
+	while (sum)
+	{
+		n = rand() % 100;
+		if (n <= 94)
+			n += 32;
+		else
+			continue;
+		if (sum - n == 0)
+		{
+			sum -= n;
+			printf("%c", n);
+		}
+		else if (sum - n - 32 > 0)
+		{
+			sum -= n;
+			printf("%c", n);
+		}
+	}
 	return (0);
 }
