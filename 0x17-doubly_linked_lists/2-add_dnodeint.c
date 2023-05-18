b8a0c8f3f954d85ae0ad2ebee33808c660e874b9
Dr-dyrane <Ogranya.Alex@gmail.com>
2023-05-18 11:28:54 -0700

    dyrane is awesome

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -7,7 +7,7 @@
  *
  *Return: Address of the new element, or NULL if it failed
  */
-dlistint_t* add_dnodeint(dlistint_t **head, const int n)
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
