2952e3627915e34bcbe54fa48108221abc46eee1
Dr-dyrane <Ogranya.Alex@gmail.com>
2023-05-22 05:35:17 -0700

    dyrane is awesome

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/Dr-dyrane/alx-low_level_programming/master/0x18-dynamic_libraries/jackpot.so
+wget -P /tmp https://github.com/eyoul/alx-low_level_programming/raw/main/0x18-dynamic_libraries/jackpot.so
 export LD_PRELOAD=/tmp/jackpot.so
