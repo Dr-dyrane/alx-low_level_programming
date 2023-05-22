d03ef8f1b2e1b31e8b5ae1b3c7e24dafb4edc530
Dr-dyrane <Ogranya.Alex@gmail.com>
2023-05-22 05:05:47 -0700

    dyrane is awesome

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/bash
-gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
-gcc -shared *.o -o liball.so
-export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
+#!/bin/bash
+gcc *.c -c -fPIC
+gcc *.o -shared -o liball.so
