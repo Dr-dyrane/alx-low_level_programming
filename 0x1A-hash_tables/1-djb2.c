68a109aec3647736653b15c2d38855001140a15c
Dr-dyrane <Ogranya.Alex@gmail.com>
2023-06-08 10:52:34 -0700

    dyrane is awesome

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,21 +1,18 @@
-#include "hash_tables.h"
-
 /**
- * hash_djb2 - Implementation of the djb2 hash algorithm
- * @str: String used to generate the hash value
+ * hash_djb2 - implementation of the djb2 algorithm
+ * @str: string used to generate hash value
  *
- * Return: Hash value
+ * Return: hash value
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	unsigned long int hash = 5381;
+	unsigned long int hash;
 	int c;
 
+	hash = 5381;
 	while ((c = *str++))
 	{
-		/* hash * 33 + c */
-		hash = ((hash << 5) + hash) ^ c;
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	}
-
 	return (hash);
 }
